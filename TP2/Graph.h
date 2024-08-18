5db3ea3c7ea6a8d318b5d86a4b3d73c6953bcb55
rodolfolopes <rodolfolopes1996@hotmail.com>
2024-08-18 13:50:37 -0300

    Corrigindo a lógica de contar subgrafos

--- TP2/Graph.h
+++ TP2/Graph.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Grafo{
     int numeroVertices;
-    map<int,list<int>> listaAdj;
+    map<int,list<int>> listaAdj;//Representação em forma de lista de adjacência
     public:
     Grafo(int num){
         numeroVertices = num;
@@ -22,55 +22,62 @@ class Grafo{
         }
          }
     }
+    //Adiciona arestas no grafo não direcionado(Aqui representado como os dois sentidos,de a pra b e de b pra a)
     void adicionaAresta(int a,int b){
         listaAdj[a].push_back(b);
         listaAdj[b].push_back(a);
     }
+    //Função para mostrar o grafo contido na instância desta classe
     void imprimir()
     {
         cout << "Grafo com número de vértices: " << numeroVertices <<endl;
         
         for (auto a : listaAdj) {
-            
+            //Vértice considerado
             cout << a.first << " -> ";
         
             for (auto b : a.second) {
-                
+                //Para onde as arestas vão do vértice considerado acima
                 cout << b << " ";
             }
             cout << endl;
         }
         
     }
-    int achaSubgrafosRec(){
-       return achaSubgrafos(numeroVertices); 
-    }
-    int achaSubgrafos(int num){
-        if(num > 1 ){
-            int expoent;
-            if(num == 2){
-                expoent = 1;
-            }else{
-                int temp = 1;
-                int contador = num;
-                while(contador>1){
-                    temp *= contador;
-                    contador--;
-                }
-                temp/= 2;
-                int temp2 = 1;
-                contador = num -2;
-                while(contador > 1){
-                    temp2 *= contador;
-                    contador--;
-                }
-                expoent = temp/temp2;
-            }
-            return num * achaSubgrafos(num - 1) + (int) pow(2,expoent);
-            
-        }else{
-            return 1;
+    //Função para achar o número total de subgrafos do grafo desta instância da classe
+    int achaSubgrafos(){
+        int resp = numeroVertices;
+       for(int a = 2;a<=numeroVertices;a++){
+        int binomioTop = 1;
+        int temp = a;
+        while(temp>1){
+            binomioTop*= temp;
+            temp--;
         }
-        
+        int binomioBottom = 1;
+        temp = a - 2;
+        while(temp>1){
+            binomioBottom*= temp;
+            temp--;
+        }
+        int pow2 = (int) pow(2,(binomioTop/binomioBottom)/2);
+        int binomioBottom2 = binomioTop;
+        binomioTop = 1;
+        temp = numeroVertices;
+        while(temp>1){
+            binomioTop*= temp;
+            temp--;
+        }
+        binomioBottom = 1;
+        temp = numeroVertices - a;
+        while(temp>1){
+            binomioBottom*=temp;
+            temp--;
+        }
+        int multiplier = (binomioTop/binomioBottom)/binomioBottom2;
+        resp+= pow2*multiplier;
+       }
+       return resp;
     }
+    
 };
--- TP2/Main.cpp
+++ TP2/Main.cpp
@@ -5,9 +5,9 @@ int main(){
     cout << "Digite o número de vértices do grafo completo desejado:(inteiro maior que zero)" << endl;
     do{
         cin >> nUsuario;
-    }while(nUsuario<=0);
+    }while(nUsuario<=0);//Validação simples do dado inserido
     Grafo g(nUsuario);
     g.imprimir();
-    cout <<"O número de subgrafos do grafo acima é: " << g.achaSubgrafosRec() << endl;
+    cout <<"O número de subgrafos do grafo acima é: " << g.achaSubgrafos() << endl;
     return 0;
 }
